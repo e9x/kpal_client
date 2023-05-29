07a6ac9ff49c50248e35ce370663eabd20d0bf6e
David Reed <reedswork@proton.me>
2023-05-28 20:20:34 -0400

    unused declarations

--- src/tools.tsx
+++ src/tools.tsx
@@ -3,22 +3,6 @@ import { extendClientSettings } from "./extendSettings.js";
 import KPalMenu from "./menu.js";
 import { waitFor } from "./util.js";
 
-declare global {
-  function pressButton(code: number): void;
-}
-
-declare let menuClassName: HTMLDivElement;
-declare let curGameInfo: HTMLDivElement;
-declare let mapInfo: HTMLDivElement;
-declare let timerVal: HTMLDivElement;
-declare let serverURL: HTMLInputElement;
-declare let inGameUI: HTMLDivElement;
-declare let endUI: HTMLDivElement;
-declare let endTimer: HTMLDivElement;
-declare let instructions: HTMLDivElement;
-declare let killsIcon: HTMLImageElement;
-declare let deathsIcon: HTMLImageElement;
-
 waitFor(
   () =>
     typeof windows === "object" &&
